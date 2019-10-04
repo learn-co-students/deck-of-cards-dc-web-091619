01affaa34b9e05c3d339d19d1dcb6dbaaa119e17
kh45 <khaledhassan45@gmail.com>
2019-10-04 09:12:26 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,41 @@
+require 'pry'
+
 class Deck
+    attr_reader :cards
+
+    @@rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    @@suit = ["Hearts", "Clubs", "Diamonds", "Spades"]
+
+    def initialize
+        @cards = []
+        @@suit.each{|suit|
+            @@rank.each{|rank|
+                @cards << Card.new(suit, rank)}}
+    end
+
+
+    # def initialize  ## my method
+    #     @cards = []
+    #     @@suit.each{|suit|
+    #         @@rank.each{|rank|
+    #             @cards << Card.new(suit, rank)}}
+    # end
+
+    def choose_card
+        #self.cards.delete(self.cards[rand(1..self.cards.length)])
+        cards.delete_at(rand(0..cards.length))
+        #binding.pry
+    end
 
 end
 
 class Card
+
+    attr_accessor :suit, :rank
+
+    def initialize(suit, rank)
+        @suit = suit
+        @rank = rank
+    end
 
 end
